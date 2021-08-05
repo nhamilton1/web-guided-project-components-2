0265d039b49955c55994bec58b8f5893368b0dc1
nhamilton1 <nickh3592@gmail.com>
2021-08-05 13:25:24 -0400

    did a lot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import { result } from 'lodash'
-
+import breeds from './breeds'
 
 // console.log(axios)
 
@@ -9,27 +9,27 @@
 
 // https://httpie.io/run --> http request is on the top. the first line is the start line. header is next, additional information about the request
 // console.log(results) // the data is not here
-console.log('1 about to fetch data with axios')
+// console.log('1 about to fetch data with axios')
 
 
-//idomatic way
+// //idomatic way
 
-axios.get('https://lambda-times-api.herokuapp.com/friends')
-  .then(futureData => {
-  //future code, for when the data acutally arrives
-  //freedom to assume the data is here
-  console.log('2 here is the future data',futureData)
-  console.log('here is the RESPONSE BODY', futureData.data)
-})
-  .catch(drama => {
-  //handle the drama
-  console.log(drama)
-})
+// axios.get('https://lambda-times-api.herokuapp.com/friends')
+//   .then(futureData => {
+//   //future code, for when the data acutally arrives
+//   //freedom to assume the data is here
+//   console.log('2 here is the future data',futureData)
+//   console.log('here is the RESPONSE BODY', futureData.data)
+// })
+//   .catch(drama => {
+//   //handle the drama
+//   console.log(drama)
+// })
 
-// if you want to handle several different promises
-//promise.all([p1, p2, p3]).then([res1, res2, res3])
+// // if you want to handle several different promises
+// //promise.all([p1, p2, p3]).then([res1, res2, res3])
 
-console.log('3 we requested data with axios')
+// console.log('3 we requested data with axios')
 // a promise is an object that represents the result of a computation
 // we need it so it doesnt block on program 
 
@@ -97,43 +97,95 @@ function dogCardMaker({ imageURL, breed }) {
 //    * ON FAILURE: log the error to the console put breakpoint
 //    * IN ANY CASE: log "done" to the console
 
-
-axios.get('https://dog.ceo/api/breed/husky/images/random/6')
-  .then(res => {
-    const images = res.data.message
-    images.forEach(image => {
-      //make doge card
-      const dogCard = dogCardMaker({imageURL: image, breed: 'husky' })
-      console.log(dogCard)
-      //append it to the DOM
-      entryPoint.append(dogCard)
-    });
-})
-  .catch(err => {
-    // debugger
-})
+// axios.get('https://dog.ceo/api/breed/samoyed/images/random/6')
+//   .then(res => { // works
+//     // console.log(res)
+//     const breed = 'samoyed';
+//     const imageURL = res.data.message[0]
+//     const dogCard = dogCardMaker({imageURL, breed})
+//     entryPoint.append(dogCard)
+//   }) 
+//   .catch(err => { //doesnt work
+//     console.error(err)
+//   }) 
 
 
-fetch('https://dog.ceo/api/breed/husky/images/random/6')
-  .then(res => {
-    // the body is not in yet, only headers
-    return res.json()
-  })
-  .then(parsedJSON => {
-    //here is the data!
-  })
-  .catch (err => {
-    debugger
-  })
+
+// .finally() // will run no matter what
+
+
+
+
+// axios.get('https://dog.ceo/api/breed/husky/images/random/6')
+//   .then(res => {
+//     const images = res.data.message
+//     images.forEach(image => {
+//       //make doge card
+//       const dogCard = dogCardMaker({imageURL: image, breed: 'husky' })
+//       console.log(dogCard)
+//       //append it to the DOM
+//       entryPoint.append(dogCard)
+//     });
+// })
+//   .catch(err => {
+//     // debugger
+// })
+
+
+// fetch('https://dog.ceo/api/breed/husky/images/random/6')
+//   .then(res => {
+//     // the body is not in yet, only headers
+//     return res.json()
+//   })
+//   .then(parsedJSON => {
+//     //here is the data!
+//   })
+//   .catch (err => {
+//     debugger
+//   })
 
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
 // that takes a breed and a count (of dogs)
 
+function getDogs(breed, number) {
+  axios.get(`https://dog.ceo/api/breed/${breed}/images/random/${number}`)
+  .then(res => { // works
+    res.data.message.forEach(imageURL => {
+      const dogCard = dogCardMaker({imageURL, breed})
+      entryPoint.append(dogCard)
+    })
+  }) 
+  .catch(err => { //doesnt work
+    console.error(err)
+  }) 
+}
+
+
+getDogs('weimaraner', 3)
 
 // 👉 (OPTIONAL) TASK 7- Put a button in index.html to 'get dogs' and add a click
 // event listener that executes `getDogs`
 
 
+const btn = document.querySelector('.get-dog');
+btn.addEventListener('click', event => {
+  const dogs = axios.get('https://lambda-times-api.herokuapp.com/breeds')
+    .then(res => {
+      res.data.forEach(breed => {
+        getDogs(breed, 3)
+      });
+    }).catch(err => console.error(err))
+})
+
+
+
 // 👉 (OPTIONAL) TASK 8- Import the breeds from `breeds.js`
 // or request them from https://lambda-times-api.herokuapp.com/breeds
 // and loop over them, fetching a dog at each iteration
+
+
+// HTTP VERB
+//C - create --> post
+//R - read --> get
+//U - update --> patch/put
+//D - delete --> deletes
